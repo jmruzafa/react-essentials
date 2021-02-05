6aa124bddac98992f0599aa35a38f86efc5a69d7
Javier M <jrruzafa@gmail.com>
2021-02-05 13:17:11 +0100

    More Unit testing

--- src/App.js
+++ src/App.js
@@ -5,28 +5,11 @@ import './App.css';
 
 
 function App({login}) {
-  const [data, setData] = useState(null);
-  const [loading, setLoading] = useState(false);
-  const [error, setError] = useState(null);
   
-  useEffect(() => {
-    if(!login) return;
-    setLoading(true);
-    fetch(`https://api.github.com/users/${login}`)
-    .then((response) => response.json())
-    .then(setData)
-    .then(() => setLoading(false))
-    .catch(setError);
-    }, [login]);
-    if(loading) return <h1>Loading...</h1>;
-    if(error) return <pre>{JSON.stringify(error, null, 2)}</pre>;
-    if(!data) return null;
 
     return (
       <div>
-        <h2>{data.name}</h2>
-        <p>{data.location}</p>
-        <img alt={data.login} src={data.avatar_url} />
+        <h2>Hello React Testing Library</h2>
       </div>
     ); 
 }
---
+++ src/App.test.js
@@ -0,0 +1,9 @@
+import { render } from "@testing-library/react";
+import React from "react";
+import App from "./App";
+
+test ("render an h2", () => {
+    const { getByText } = render(<App />);
+    const h2 = getByText(/Hello React Testing Library/);
+    expect(h2).toHaveTextContent("Hello React Testing Library");
+})
